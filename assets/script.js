cdb56e0e5f1791995a25e68af47384036ae370ab
Peyton Mastropolo <135174286+pmastropolo@users.noreply.github.com>
2023-07-19 13:52:16 -0500

    Update script.js
    adding function to update time & display on page

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,9 @@ $(document).ready(function() {
   var descriptionInputs = $('.description');      // Class with .description / goes with this line textarea class="col-10 description" 
   var resetBtn = $('#resetBtn');                   // Reset Button /at the bottom of page / clear schedule 
 
-                  
+// function ticktockontheclock should update current time on the page 
+  function tickTockOnTheClock() {                    
+    var currentDay = dayjs().format('YYYY-MM-DD HH:mm:ss');       // Using day.js format YYYY-MM-DD HH:mm:ss
+    currentDayP.text(currentDay);                               //   .text() jQuery method will set text of an element, will update p id to display time 
+  }
+  
