fe81b065a7c403388baa5de02dd8476f02afcc33
Peyton Mastropolo <135174286+pmastropolo@users.noreply.github.com>
2023-07-19 13:40:41 -0500

    Update script.js
    adding variables

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,9 @@
 $(document).ready(function() {
+  // Variables 
+  var currentDayP = $("#currentDay");             // P id to display current time on page / goes with this line p id="currentDay"
+  var timeBlocks = $('.time-block');              // class .time-block / goes with this line div class="row time-block
+  var saveBtns = $('.saveBtn');                    // Save Button / goes with this line button class="btn saveBtn
+  var descriptionInputs = $('.description');      // Class with .description / goes with this line textarea class="col-10 description" 
+  var resetBtn = $('#resetBtn');                   // Reset Button /at the bottom of page / clear schedule 
 
-/* Important: Current Day Needs to be Displayed at the Top of Calendar!
-  It Should Look Like Month, Day Year Ex: April 10, 2022
-  Using dayjs will give us the current day and format
- */
-var currentDay = dayjs().format('MMMM D, YYYY');
-    
-// This will show current day at top of page. 
-$("#currentDay").text(currentDay);
-  
-// Current Hour
-var currentHour = dayjs().hour();
-
-// Add event listener to buttons
-$('.add-event').on('click', function() {
-
-
-});
-
-// Save event when save button is clicked function 
-$(".saveBtn").click(function() {
-
-
-
-});
-});
-
-    
-
-
-/*
-  Classes:
-- Save Button class saveBtn
-- Text Area Class Is Description
-- hour is hour class? ( div class="col-2 col-md-1 hour text-center py-3">9AM/div )
-- row or time-block is class? (div id="hour-9" class="row time-block"> )
-- class = container-fluid or just container?
-
-*/
+                  
